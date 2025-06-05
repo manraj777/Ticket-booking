bae99bc961056706fb2457c1802be5d926d3d3e5
manraj777 <manraj.engg@gmail.com>
2025-06-05 19:44:50 +0530

    user with id pswd can login

--- packages/tests/index.spec.ts
+++ packages/tests/index.spec.ts
@@ -1,5 +1,25 @@
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
+import axios from "axios";
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(3).toBe(3);
+const BACKEND_URL = 'http://localhost:8080';
+
+const USERNAME1 = 'testuser';
+const USERNAME2 = 'testuser2';
+
+const PASSWORD1 = 'testpassword';
+const PASSWORD2 = 'testpassword2';
+
+describe("Signup endpoint", () => {
+    test('Double signup doesnt work', async () => {
+    const response1 = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+        username: USERNAME1,
+        password: PASSWORD1
+    })
+     const response2 = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+        username: USERNAME1,
+        password: PASSWORD1
+    })
+    expect(response1.status).toBe(200);
+    expect(response1.status).toBe(403);
 });
+})
