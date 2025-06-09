11e372d3956a2b9f947955c45a6509c709453bd3
manraj777 <manraj.engg@gmail.com>
2025-06-09 23:46:32 +0530

    Added singup endpointdb calls

--- apps/http-backend/src/routes/v1/user.ts
+++ apps/http-backend/src/routes/v1/user.ts
@@ -1,31 +1,65 @@
 import { Router } from "express";
 import { verifyToken, generateToken } from "authenticator";
-
+import { client } from "@repo/db/client";
+import jwt from "jsonwebtoken";
+import { JWT_PASSWORD } from "../../config";
  const router : Router = Router();
 
- router.post("/signup", (req, res) => {
-    const phoneNumber = req.body.phoneNumber;
-    const totp = generateToken(phoneNumber + "SIGNUP");
+ router.post("/signup", async(req, res) => {
+    const number = req.body.phoneNumber;
+    const totp = generateToken(number + "SIGNUP");
     // send totp to phone number
+    const user = await client.user.upsert({
+        where:{
+            number
+        },
+        create: {
+            number,
+            name: ""
+        },
+        update: {
+            
+        }
+    });
+
+    if(process.env.NODE_ENV === "production"){
+        // send otp to user's phone number 
+    }
+   
 
 
     res.json({
-        id : "1"
+        id : user.id
     })
  });
 
- router.post("/signup/verify", (req, res) => {
-    const phoneNumber = req.body.phoneNumber;
-    const totp = generateToken(phoneNumber + "SIGNUP");
-    if(!verifyToken(phoneNumber + "SIGNUP", req.body.otp)){
+ router.post("/signup/verify", async(req, res) => {
+    const number = req.body.phoneNumber;
+    const name = req.body.name;
+    const totp = generateToken(number + "SIGNUP");
+    if(!verifyToken(number + "SIGNUP", req.body.otp)){
         res.json({
             message : "Invalid OTP"
         })
         return;
     }
+
+    const userId = await client.user.update({
+        where: {
+            number
+        },
+        data: {
+            name,
+            verified: true
+        }
+    })
+    const token = jwt.sign({
+        userId
+    }, JWT_PASSWORD)
     // set user to verified in database
+
     res.json({
-        
+        token
     });
  });
 
