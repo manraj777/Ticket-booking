65b910176af1fa495cf43411a526601caabacd48
manraj777 <manraj.engg@gmail.com>
2025-07-07 23:57:30 +0530

    test controller added

--- apps/http-backend/src/controllers/test/index.ts
+++ apps/http-backend/src/controllers/test/index.ts
@@ -1,14 +1,7 @@
-import { Router } from "express";
 import { client, AdminType } from "@repo/db/client";
 import jwt from "jsonwebtoken";
-import { ADMIN_JWT_PASSWORD } from "../../../config";
-
-const router: Router = Router();
-
-router.post("/create-admin" , async (req, res) => {
-    const number = req.body.number;
-    const name = req.body.name;
-
+import { ADMIN_JWT_PASSWORD } from "../../config";
+export async function createAdmin(number: string, name: string){
     const admin = await client.admin.create({
         data: {
             number,
@@ -21,8 +14,4 @@ router.post("/create-admin" , async (req, res) => {
     const token = jwt.sign({
         userId: admin.id,
     },ADMIN_JWT_PASSWORD)
-    res.json({
-        id: admin.id, 
-        token
-    })
-});
+}
