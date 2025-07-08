308dec58f4d253e47ed42bbfdd3f13fd427566f8
manraj777 <manraj.engg@gmail.com>
2025-07-08 23:48:09 +0530

    events created

--- apps/http-backend/src/routes/v1/admin/event.ts
+++ apps/http-backend/src/routes/v1/admin/event.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { client } from "@repo/db/client";
 import { adminMiddleware } from "../../../middleware/admin";
-import { CreateEventSchema } from "@repo/common/types";
+import { CreateEventSchema, UpdateEventSchema } from "@repo/common/types";
 import { getEvent } from "../../../controllers/events";
 
 const router: Router = Router();
@@ -43,6 +43,54 @@ router.post("/", adminMiddleware, async (req, res) => {
             error: "Internal server error"
         });
     }
+});
+
+
+router.put("/:eventId", adminMiddleware, async (req, res) => {
+    const {data, success} = UpdateEventSchema.safeParse(req.body);
+    const adminId = req.userId;
+    const eventId = req.params.eventId ?? "";
+
+    if(!adminId) {
+        res.status(401).json({ 
+            error: "Unauthorized: adminId missing" 
+        });
+        return;
+    }
+
+    if(!success) {
+        res.status(400).json({
+            error: "Invalid request body"
+        });
+        return;
+    }
+
+    try {
+        const event = await client.event.update({
+            where: {
+                id: eventId,
+                adminId: adminId
+            },
+            data: {
+                name: data.name,
+                description: data.description,
+                startTime: new Date(data.startTime),
+                locationId: data.location,
+                banner: data.banner,
+                adminId,
+                published: data.published,
+                ended: data.ended
+            }   
+        })
+        res.json({
+            id : event.id
+        })
+    } catch(e) {
+        res.status(500).json({
+            messsage : "Could not update event",
+        })
+    }
+
 });
 
 router.get("/events", adminMiddleware, async (req, res) => {
